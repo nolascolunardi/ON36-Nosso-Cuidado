487879a5cff74cc789fcf916e6a40c6d3bf3bf3a
Ana Laura Nolasco <nolascolunardi@gmail.com>
2024-10-13 13:06:55 -0300

    feat: nest project setup inicial

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import 'reflect-metadata';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { config } from 'dotenv';
+
+config();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  await app.listen(3000);
+}
+bootstrap();
