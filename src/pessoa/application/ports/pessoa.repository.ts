b5cf53945298cf772ebe4280df9646c0bf250ba7
Ana Laura Nolasco <nolascolunardi@gmail.com>
2024-10-18 00:39:14 -0300

    feat: adicionado pessoa gestante repository

---
+++ src/pessoa/application/ports/pessoa.repository.ts
@@ -0,0 +1,11 @@
+import { Pessoa } from '../../domain/pessoa.entity';
+import { AtualizarPessoaDto } from '../../presenter/dto/atualizar-pessoa.dto';
+
+export abstract class PessoaRepository {
+  abstract salvar(pessoa: Pessoa): Promise<Pessoa>;
+  abstract listarTodas(): Promise<Pessoa[]>;
+  abstract atualizar(id: string, pessoa: Partial<AtualizarPessoaDto>): Promise<Pessoa>;
+  abstract deletar(email: string): Promise<void>;
+  abstract buscarPorCpf(cpf: string): Promise<Pessoa>;
+  abstract buscarPorEmail(email: string): Promise<Pessoa>;
+}
---
+++ src/pessoa/infrastructure/typeOrm/pessoaTypeOrm.repository.ts
@@ -0,0 +1,51 @@
+import { PessoaRepository } from '../../application/ports/pessoa.repository';
+import { Pessoa } from '../../domain/pessoa.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AtualizarPessoaDto } from '../../presenter/dto/atualizar-pessoa.dto';
+
+export class PessoaTypeOrmRepository extends PessoaRepository {
+  constructor(
+    @InjectRepository(Pessoa)
+    private readonly pessoaRepository: Repository<Pessoa>,
+  ) {
+    super();
+  }
+
+  async salvar(pessoa: Pessoa): Promise<Pessoa> {
+    return await this.pessoaRepository.save(pessoa);
+  }
+
+  async atualizar(id: string, pessoa: Partial<AtualizarPessoaDto>): Promise<Pessoa> {
+    await this.pessoaRepository.update({ id }, pessoa);
+    return await this.pessoaRepository.findOne({
+      where: { id },
+      relations: ['endereco', 'pontuacao'],
+    });
+  }
+
+  async listarTodas(): Promise<Pessoa[]> {
+    return await this.pessoaRepository.find({
+      relations: ['endereco', 'pontuacao'],
+      where: { isAtivo: true },
+    });
+  }
+
+  async deletar(cpf: string): Promise<void> {
+    await this.pessoaRepository.update({ cpf }, { isAtivo: false });
+  }
+
+  async buscarPorCpf(cpf: string): Promise<Pessoa> {
+    return await this.pessoaRepository.findOne({
+      where: { cpf, isAtivo: true },
+      relations: ['endereco', 'pontuacao'],
+    });
+  }
+
+  async buscarPorEmail(email: string): Promise<Pessoa> {
+    return await this.pessoaRepository.findOne({
+      where: { email, isAtivo: true },
+      relations: ['endereco', 'pontuacao'],
+    });
+  }
+}
