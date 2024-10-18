6be21da4a24d792652a864794f2b4c94e697f3f8
Ana Laura Nolasco <nolascolunardi@gmail.com>
2024-10-18 01:07:52 -0300

    feat: adicionado consulta controller com metodos de criar, atualizar status, listar todas consultas e buscar por id

---
+++ src/consulta/presenter/consulta.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Get, Post, Body, Param, Put, Patch } from "@nestjs/common";
+import { ConsultaService } from '../application/consulta.service';
+import { CriarConsultaDto } from './dto/criar-consulta.dto';
+import { AtualizarConsultaDto } from './dto/atualizar-consulta.dto';
+
+@Controller('consultas')
+export class ConsultaController {
+  constructor(private readonly consultaService: ConsultaService) {}
+
+  @Post()
+  criar(@Body() criarConsulta: CriarConsultaDto) {
+    return this.consultaService.criar(criarConsulta);
+  }
+
+  @Patch('/:id')
+  atualizarStatus(
+    @Param('id') id: string,
+    @Body() atualizarDto: AtualizarConsultaDto,
+  ) {
+    return this.consultaService.atualizarStatus(id, atualizarDto);
+  }
+
+  @Get('pessoa/:email')
+  listarTodas(@Param('email') email: string) {
+    return this.consultaService.listarTodas(email);
+  }
+
+  @Get('/:id')
+  buscarPorId(@Param('id') id: string) {
+    return this.consultaService.buscarPorId(id);
+  }
+}
