8397458f6182aff63d6932192135fcf4d3293219
Ana Laura Nolasco <nolascolunardi@gmail.com>
2024-10-19 00:40:40 -0300

    refact: retornado parametros como dtos corretamente, modificado json para snake case

--- src/consulta/presenter/consulta.controller.ts
+++ src/consulta/presenter/consulta.controller.ts
@@ -10,13 +10,15 @@ import {
 import { ConsultaService } from '../application/consulta.service';
 import { CriarConsultaDto } from './dto/criar-consulta.dto';
 import { AtualizarConsultaDto } from './dto/atualizar-consulta.dto';
+import { ConsultaDTO } from './dto/consulta.dto';
+import { Consulta } from "../domain/consulta.entity";
 
 @Controller('consultas')
 export class ConsultaController {
   constructor(private readonly consultaService: ConsultaService) {}
 
   @Post()
-  criar(@Body() criarConsulta: CriarConsultaDto) {
+  criar(@Body() criarConsulta: CriarConsultaDto): Promise<ConsultaDTO> {
     return this.consultaService.criar(criarConsulta);
   }
 
@@ -24,27 +26,28 @@ export class ConsultaController {
   atualizarStatus(
     @Param('id') id: string,
     @Body() atualizarDto: AtualizarConsultaDto,
-  ) {
+  ): Promise<ConsultaDTO> {
     return this.consultaService.atualizarStatus(id, atualizarDto);
   }
 
   @Get('')
-  listarTodas() {
+  listarTodas(): Promise<ConsultaDTO[]> {
     return this.consultaService.listarTodas();
   }
 
   @Get('/pessoa/:email')
-  listarTodosPorEmail(@Param('email') email: string) {
+  listarTodosPorEmail(@Param('email') email: string): Promise<ConsultaDTO[]> {
     return this.consultaService.listarTodasPorEmail(email);
   }
 
   @Get('/:id')
-  buscarPorId(@Param('id') id: string) {
+  buscarPorId(@Param('id') id: string): Promise<Consulta> {
     return this.consultaService.buscarPorId(id);
   }
 
   @Delete('/:id')
-  deletar(@Param('id') id: string) {
-    return this.consultaService.deletar(id);
+  async deletar(@Param('id') id: string): Promise<string> {
+    await this.consultaService.deletar(id);
+    return 'Consulta deletada com sucesso!';
   }
 }
--- src/consulta/presenter/dto/consulta.dto.ts
+++ src/consulta/presenter/dto/consulta.dto.ts
@@ -1,19 +1,19 @@
 import { Consulta } from '../../domain/consulta.entity';
 
 export class ConsultaDTO {
-  consultaId: string;
-  data: Date;
-  hora: string;
+  consulta_id: string;
   pessoa: string;
   status: string;
+  data: Date;
+  hora: string;
   tipo: string;
 
   constructor(entity: Consulta) {
-    this.consultaId = entity.id;
-    this.data = entity.dataConsulta;
-    this.hora = entity.horaConsulta;
+    this.consulta_id = entity.id;
     this.pessoa = entity.pessoa.nomeCompleto;
     this.status = entity.status;
+    this.data = entity.dataConsulta;
+    this.hora = entity.horaConsulta;
     this.tipo = entity.tipoConsulta;
   }
 }
