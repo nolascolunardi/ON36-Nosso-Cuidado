9f093a12bf5319c03721122e003e1a52d90fc835
Ana Laura Nolasco <nolascolunardi@gmail.com>
2025-02-03 23:25:22 -0300

    test: cobertura de testes unitários para PessoaService

--- test/__mocks__/pessoa.mock.ts
+++ test/__mocks__/pessoa.mock.ts
@@ -1,4 +1,49 @@
 import { Pessoa } from '../../src/pessoa/domain/pessoa.entity';
+import { CriarPessoaDto } from "../../src/pessoa/presenter/dto/criar-pessoa.dto";
+import { PessoaDto } from "../../src/pessoa/presenter/dto/pessoa.dto";
+import { enderecoEntidade } from "./endereco.mock";
+
+export const criarPessoaDto = {
+  nome: 'Nome Completo',
+  email: 'pessoa@ex.com',
+  senha: '123456',
+  telefone: '11999999999',
+  cpf: '12345678901',
+  dataNascimento: '2021-09-01',
+  semanasGestacao: 10,
+  medicoResponsavel: 'Dr. Médico',
+  cep: '12345678',
+  logradouro: 'Rua Teste',
+  numero: '123',
+  bairro: 'Bairro Teste',
+  cidade: 'Cidade Teste',
+  estado: 'SP',
+  pais: 'Brasil',
+  toEntity: () => pessoaEntidade,
+  toEnderecoDto: () => enderecoEntidade
+}as unknown as CriarPessoaDto;
+
+export const pessoaEntidade = {
+  nomeCompleto: 'Nome Completo',
+  email: 'pessoa@ex.com',
+  senha: '123456',
+  telefone: '11999999999',
+  cpf: '12345678901',
+  dataNascimento: '2021-09-01',
+  semanasGestacao: 10,
+  medicoResponsavel: 'Dr. Médico',
+  endereco: {
+    cep: '12345678',
+    logradouro: 'Rua Teste',
+    numero: '123',
+    bairro: 'Bairro Teste',
+    cidade: 'Cidade Teste',
+    estado: 'SP'
+  },
+  pontuacao: {
+    pontos: 0
+  }
+}as unknown as Pessoa;
 
 export const pessoaMock = {
   id: '147a147aaaaaaa',
@@ -14,7 +59,6 @@ export const pessoaMock = {
     cep: '12345678',
     logradouro: 'Rua Teste',
     numero: '123',
-    complemento: 'apto 123',
     bairro: 'Bairro Teste',
     cidade: 'Cidade Teste',
     estado: 'SP'
@@ -23,3 +67,5 @@ export const pessoaMock = {
     pontos: 0
   }
 } as unknown as Pessoa;
+
+export const pessoaDto = new PessoaDto(pessoaMock);
